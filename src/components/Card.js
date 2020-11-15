63462c20694cb491addcaa742c325ae5485c02c6
Ryan Chase <ryan@chasery.com>
2020-11-15 11:33:15 -0800

    Add in base components and styles

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Card.css";
+
+function Card() {
+  return (
+    <div className="Card">
+      <button type="button">delete</button>
+      <h3>Eleventh card</h3>
+      <p>lorem ipsum</p>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/List.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Card.js";
+import "./List.css";
+
+function List() {
+  return (
+    <section className="List">
+      <header className="List-header">
+        <h2>Second list</h2>
+      </header>
+      <div className="List-cards">
+        <Card />
+        <button type="button" className="List-add-button">
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default List;
